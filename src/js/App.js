6f6abbc483c02c840886cd2c4c43d3c763bc6123
kaiming Liu <kai0919@hotmail.com>
2019-05-07 14:07:30 +0800

    first commit

---
+++ src/js/App.js
@@ -0,0 +1,26 @@
+import $ from 'jquery'
+// import {goSubmit, checkinfo, s, f, QueryString} from '../services/validate'
+import React from 'react';
+
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {}
+
+    }
+
+    componentDidMount() {
+        console.log('hello, world!!!');
+    }
+
+    render() {
+        return (
+            <div>hello!</div>
+        )
+    }
+}
+
+export default App;
